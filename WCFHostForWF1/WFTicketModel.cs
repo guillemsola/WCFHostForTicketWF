fb8a8728b013ecaa84343ab2900df5e067cc9433
guillemsola <guillemsola@gmail.com>
2013-10-13 20:16:58 +0200

    First steps with correlation working properly

---
+++ WCFHostForWF1/WFTicketModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFHostForWF1
+{
+    public class WFTicketModel
+    {
+        public Guid TicketId { get; set; }
+    }
+}
