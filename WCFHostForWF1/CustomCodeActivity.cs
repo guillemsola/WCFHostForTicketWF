3efa0b9a0c55086f759dcc3e4220d7833772a5fd
guillemsola <guillemsola@gmail.com>
2013-10-20 17:13:09 +0200

    Ass state machine with parallel control POC.
    Add system activities library for new tests (web api)
    Test persistence ok.

---
+++ WCFHostForWF1/CustomCodeActivity.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomCodeActivity.cs" company="None">
+//   None
+// </copyright>
+// <summary>
+//   The custom code activity.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WCFHostForWF1
+{
+    using System;
+    using System.Activities;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// The custom code activity.
+    /// </summary>
+    public sealed class CustomCodeActivity : CodeActivity
+    {
+        // Define an activity input argument of type string
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the text.
+        /// </summary>
+        public InArgument<string> Text { get; set; }
+
+        #endregion
+
+        // If your activity returns a value, derive from CodeActivity<TResult>
+        // and return the value from the Execute method.
+        #region Methods
+
+        /// <summary>
+        /// The execute.
+        /// </summary>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        protected override void Execute(CodeActivityContext context)
+        {
+            // Obtain the runtime value of the Text input argument
+            var text = context.GetValue(this.Text);
+            Debug.Print(string.Format("Changing WF status{0}", text));
+        }
+
+        #endregion
+    }
+}
--- WCFHostForWF1/WFTicketModel.cs
+++ WCFHostForWF1/WFTicketModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WFTicketModel.cs" company="None">
+//   None
+// </copyright>
+// <summary>
+//   The wf ticket model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace WCFHostForWF1
 {
+    using System;
+
+    /// <summary>
+    /// The WF ticket model.
+    /// </summary>
     public class WFTicketModel
     {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the ticket id.
+        /// </summary>
         public Guid TicketId { get; set; }
+
+        #endregion
     }
 }
